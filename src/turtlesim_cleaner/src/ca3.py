fd9eb419f67cfb37081db320bbbbd5ed6f898afc
Brandon Bruen <bbruen3@BrandonsOldMac.fios-router.home>
2020-04-24 15:48:44 -0400

    First pass spiral

--- src/turtlesim_cleaner/src/ca3.py
+++ src/turtlesim_cleaner/src/ca3.py
@@ -194,7 +194,43 @@ class TurtleBot:
         angular = 360/time
         angular_vel = angular*2*PI/360
 
-        iteration = 0
+        vel_msg = Twist()
+        
+        vel_msg.linear.x = abs(speed)
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = angular_vel
+
+        #while not rospy.is_shutdown():
+
+        #Setting the current time for distance calculus
+        t0 = rospy.Time.now().to_sec()
+        current_angle = 0
+        total_angle = 360*rotations
+            #Loop to move the turtle in an specified distance
+        while(current_angle < total_angle):
+                #Publish the velocity
+            self.velocity_publisher.publish(vel_msg)
+                #Takes actual time to velocity calculus
+            t1=rospy.Time.now().to_sec()
+                #Calculates distancePoseStamped
+            current_angle = angular*(t1-t0)
+            #After the loop, stops the robot
+        vel_msg.linear.x = 0
+        vel_msg.linear.y = 0
+        vel_msg.angular.z = 0
+            #Force the robot to stop
+        self.velocity_publisher.publish(vel_msg)
+        return
+
+    def spiral(self, radius, speed=5, rotations=1):
+        distance = radius * PI 
+        time = distance/speed
+        angular = 360/time * 0.9
+        angular_vel = angular*2*PI/360
+
         vel_msg = Twist()
         
         vel_msg.linear.x = abs(speed)
